e3f601d13eea7c94338c27be7c60f34d43cd3c05
AndreNasc84 <andre.nasc84@gmail.com>
2022-05-28 10:51:32 -0300

    fonte aula de sexta... fonte js, aula sab teste

---
+++ primeiro.js
@@ -0,0 +1,25 @@
+function SOMA(NUM1, NUM2){
+    return NUM1 + NUM2;
+}
+
+var Nome = "André do Nascimento";
+var Formacao = "- Avanade PCDev -";
+var Ano = 2022;
+
+function DIVISAO(NUM1, NUM2){
+    return NUM1 / NUM2;
+}
+
+function NOME(André){
+    return NOME;
+}
+
+let Nome = ['Andre', 'Erick', 'Nilce', 'Cassia', 'Felipe', 'Moises', 'Maiara'];
+
+
+/*1 - Crie uma função que retorne o seu nome
+2 - Crie uma função que retorne o nome passado por parâmetro
+3 - Faça uma função para dividir 2 números
+4 - faça uma função que retorne um array com o nome dos alunos presentes na aula
+5 - faça uma função que recebe um array e um valor, adicione o valor no array e retorne o array
+*/
