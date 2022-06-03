fa872151ce67f582316f9812c579856eb7496eeb
AndreNasc84 <andre.nasc84@gmail.com>
2022-06-03 18:36:42 -0300

    node 2

---
+++ node 2/index.js
@@ -0,0 +1,80 @@
+const express = require('express') /*require é uma função global no node, busca no node_nodules por biblioteca informado como parametro, onde instancia o objeto*/ 
+const app = express()
+
+function helloNode(req, res){
+    res.send('Hello Word');
+}
+
+function somarNumeros(req, res){
+    const n1 = 2;
+    const n2 = 1;
+    res.send((n1 + n2).toString());
+}
+
+function retornaDigitado(req, res){ /*req= requisição, res= response/resultado*/
+    const digitado = req.params.id;
+    res.send(digitado);
+}
+
+function retornaFrasePokemon(req, res){
+    const pokemon = req.params.nomePokemon;
+    res.send(`O pokemon digitado foi o ${pokemon}`);
+}
+
+app.get('/teste/andre',helloNode);
+app.get('/', somarNumeros);
+app.get('/:id', retornaDigitado);
+app.get('/pokemon/:nomePokemon', retornaFrasePokemon);
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+/*
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const list = [];
+
+function getObj(req, res){
+    res.send(lista);
+}
+
+function criandoObj(req, res){
+    const obj = req.body;
+    lista.push(obj);
+    res.send('inserido com sucesso');
+}
+
+app.get('/objt', getObj);
+app.post('/criando', criandoObj);
+
+app.listen(3000);
+
+
+
+/***
+ * Crie um projeto node novo junto com express e body parser.
+Crie uma lista no arquivo index.js para receber valores.
+Crie uma rota GET /pokemon que busque a lista anterior.
+Crie uma rota chamada POST /pokemon para inserir na lista anterior o pokemon, com os seguintes atributos:
+name: string
+id: int
+default: boolean
+weight: double (ponto flutuante ou número com casa decimal)
+order: int,
+nicknames: lista de string com apelidos do pokemon.
+ * 
+ * 
+ * 
+ */
