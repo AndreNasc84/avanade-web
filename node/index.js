e949d7f5ed24211b72e7986bb0a103c89047489f
AndreNasc84 <andre.nasc84@gmail.com>
2022-06-03 19:07:11 -0300

    atividade atrasada

--- node/index.js
+++ node/index.js
@@ -35,27 +35,57 @@ function getFilhosMaisUmAnoIdade(){
 console.log(getFilhosMaisUmAnoIdade());
 
 
-/**** */
-function teste () {
-    return {nome: "Marcos",
-    escrevernome: function (){
-    console.log(this.nome);
-    },
-    mae: {nome: "Iza", idade: 62}, 
-    idade: 25,
-    habilitado: false,
-    filhos: [{nome: "José", idade: 50}, {nome: "Maria", idade: 0}],
-    cursos: ["JS", "JAVA", "C#"], 
-    corCabelo: "Preto", 
-    altura: 1.75
+/******************************************* */
+/*
+1- Crie uma função dentro do arquivo index.js que imprima no console o seguinte objeto:
+
+{
+        altura: 1.75,
+        corCabelo: "preto",
+        cursos: ["JS", "JAVA", "C#"],
+        nome: "Marcos",
+        escreverNome: function () {
+            console.log(this.nome);
+        },
+        filhos: [{ nome: "José", idade: 50 }, { nome: "Maria", idade: 0 }],
+        habilitado: false,
+        idade: 25,
+        mae: { nome: "Iza", idade: 62 }
+    };
+
+
+execute com npm run start para ver o resultado no console.
+*/
+
+
+function imprima () {
+    return {
+        nome: "Marcos",
+        idNome: function (){
+            console.log(this.nome);
+        },
+        mae: {nome: "Iza", idade: 62}, 
+        idade: 25,
+        habilitado: false,
+        filhos: [{nome: "José", idade: 50}, {nome: "Maria", idade: 0}],
+        cursos: ["JS", "JAVA", "C#"], 
+        corCabelo: "Preto", 
+        altura: 1.75
     };
 }
-console.log(teste());
 
-function filhoscommaisdeumano () {
+console.log(imprima());
+
+/*
+
+2 - Crie uma função no arquivo index.js que utilize a função anterior e filtre pelos filhos com mais de 1 ano de idade.
+execute npm run start para ver o resultado
+*/
+
+function filhoMaisUmAno() {
     return teste().filhos.filter((filhos) => {return filhos.idade > 1});
 }
-console.log(filhoscommaisdeumano());
+console.log(filhoMaisUmAno());
 
 
 function parouimpar (n1){
---
+++ node/index2.js
@@ -0,0 +1,17 @@
+/*Atividade:
+Faça no arquivo index.js criado na aula de nodejs, uma função que receba como parâmetro 1 número e identifique se esse número é par ou ímpar (pesquisar na internet sobre o operador MOD representado pelo dígito %). O retorno precisa ser um boolean.
+Utilize o console.log e chame a função criada no arquivo index.js. Execute npm run start para rodar o código e ver o resultado no console.
+*/
+
+function ePar (num1){
+    var num1 = 7
+        console.log(`O número ${num1} é par?`)
+    
+        if (num1 % 2 == 0){
+            return true
+        }
+            else{
+            return false
+    };
+}
+console.log(ePar());
