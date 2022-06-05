22512c0f0a4a99d0e48f9cc63e9443dfcba7c059
AndreNasc84 <andre.nasc84@gmail.com>
2022-06-05 20:10:43 -0300

    revisao

--- node/index2.js
+++ node/index2.js
@@ -1,5 +1,6 @@
 /*Atividade:
-Faça no arquivo index.js criado na aula de nodejs, uma função que receba como parâmetro 1 número e identifique se esse número é par ou ímpar (pesquisar na internet sobre o operador MOD representado pelo dígito %). O retorno precisa ser um boolean.
+Faça no arquivo index.js criado na aula de nodejs, uma função que receba como parâmetro 1 número e identifique se esse número é par ou ímpar 
+(pesquisar na internet sobre o operador MOD representado pelo dígito %). O retorno precisa ser um boolean.
 Utilize o console.log e chame a função criada no arquivo index.js. Execute npm run start para rodar o código e ver o resultado no console.
 */
 
